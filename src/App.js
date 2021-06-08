59294fbc94885169cac454cca618cb8e0d70bf24
VanillaTank <tedien2016@yandex.ru>
2021-06-08 11:05:30 +0800

    изменила кнопки на карточках, карточки - в отдельн компоненту

--- src/App.js
+++ src/App.js
@@ -12,23 +12,24 @@ import Archive from './components/Archive/Archive'
 function App(props) {
   let tascRoute = props.state.dataProject.map(el => {
     return (
-      <Route key={Math.random()} path={"/" + el.id} render ={ () =><TaskPage taskData={el} data={props.state.dataProject}/>} />
-    )})
-  let doneProjects = props.state.dataProject.filter(task => task.complete && !task.isArchive)  
-  let inProcessProjects = props.state.dataProject.filter(task => !task.complete  && !task.isArchive) 
+      <Route key={Math.random()} path={"/" + el.id} render={() => <TaskPage taskData={el} data={props.state.dataProject} />} />
+    )
+  })
+  let doneProjects = props.state.dataProject.filter(task => task.complete && !task.isArchive)
+  let inProcessProjects = props.state.dataProject.filter(task => !task.complete && !task.isArchive)
   let archivProjects = props.state.dataProject.filter(task => !task.complete && task.isArchive)
-  
+
 
   return (
     <BrowserRouter>
       <div>
-        
-        <Route exact path={"/"} render ={ () =>< MainScreen data={inProcessProjects} dispatch={props.dispatch}/>}  />
+
+        <Route exact path={"/"} render={() => < MainScreen data={inProcessProjects} dispatch={props.dispatch} />} />
         {tascRoute}
-        <Route path={"/create"} render ={ () =><Create newTaskData={props.state.newTaskData} dispatch={props.dispatch}/>}  />
-        <Route path={"/done-projects"} render ={ () =><DoneProjects doneProjects={doneProjects} dispatch={props.dispatch}/>}  />
-        <Route path={"/archive"} render ={ () =><Archive dispatch={props.dispatch} archivProjects={archivProjects} />}  />
-        
+        <Route path={"/create"} render={() => <Create newTaskData={props.state.newTaskData} dispatch={props.dispatch} />} />
+        <Route path={"/done-projects"} render={() => <DoneProjects doneProjects={doneProjects} dispatch={props.dispatch} />} />
+        <Route path={"/archive"} render={() => <Archive dispatch={props.dispatch} archivProjects={archivProjects} />} />
+
         <Footer />
       </div>
     </BrowserRouter>
--- src/components/DoneProjects/DoneProjects.jsx
+++ src/components/DoneProjects/DoneProjects.jsx
@@ -1,45 +1,22 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import {moveCardToArchive, deleteCardProject, moveInNotDone} from '../../redux/state'
+import CardProject from '../CardProject/CardProject'
+
 
 const DoneProjects = (props) => {
 
-    let moveToArchive = (e) => {
-        e.preventDefault();
-        let cardID = e.currentTarget.closest('.cardLi').id;
-        props.dispatch( moveCardToArchive( cardID ) )
-    }
-
-    let deleteCard = (e) => {
-        e.preventDefault();
-        let cardID = e.currentTarget.closest('.cardLi').id;
-        props.dispatch( deleteCardProject( cardID ) )
-    }
-
-    let moveToActive = (e) => {
-        e.preventDefault();
-        let cardID = e.currentTarget.closest('.cardLi').id;
-        props.dispatch( moveInNotDone( cardID ) )
-    }
-
     let liData = props.doneProjects.map(el => {
-        return (<li key={el.id} id={el.id} className="cardLi">
-            <div>
-                <NavLink to={"/" + el.id}>{el.title}</NavLink>
-                <button onClick={moveToActive}>Не сделано</button>
-                <button>редактировать</button>
-                <button onClick={moveToArchive}>в архив</button>                
-                <button onClick={deleteCard}>удалить</button>
-            </div>
-        </li> )
+        return (<li key={el.id} className="cardLi" id={el.id}>
+            <CardProject el={el} dispatch={props.dispatch} whoCall={'DoneProjects'} />
+        </li>)
     })
 
     return (
-            <div>
-                <h2>Список завершенных дел</h2>
-                <ul>{liData}</ul>
-                <NavLink to="/create">Добавить новый проект</NavLink>
-            </div>
+        <div>
+            <h2>Список завершенных дел</h2>
+            <ul>{liData}</ul>
+            <NavLink to="/create">Добавить новый проект</NavLink>
+        </div>
     )
 }
 
