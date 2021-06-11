565f689f8f9095f3a2c554713a0f345d2b199ae0
VanillaTank <tedien2016@yandex.ru>
2021-06-11 16:48:19 +0800

    не помню, что изменила (((

--- src/components/TaskPage/TaskPage.jsx
+++ src/components/TaskPage/TaskPage.jsx
@@ -1,17 +1,44 @@
 import React from 'react'
 import AddTask from './../AddTask/AddTask'
 import Sidebar from './../Sidebar/Sidebar'
-
+import style from './TaskPage.module.css'
 
 
 const TaskPage = (props) => {
-    console.log(props);
-    let activeTasks = props.taskData.tasks.filter(el => el.complete === "false")
+    let editTitle = (e) => {
+        e.preventDefault();
+        let title = e.currentTarget;
+        let titleText = title.innerText;
+        title.innerHTML = '<input type="text" placeholder="' + titleText + '"/>'
+    }
+
+    let editTitleOnBlur = (e) => {
+        e.preventDefault();
+        let title = e.currentTarget;
+        title.innerHTML = title.value;
+    }
     
+    let editDescription = (e) => {
+        e.preventDefault();
+        console.log(2);
+    }
+
+    let editTextOfTask = (e) => {
+        e.preventDefault();
+        console.log(3);
+        console.log(e.currentTarget);
+    }
+
+    let toggleCheckboxTasks = (e) => {
+        console.log(4);
+    }
+
+    let activeTasks = props.taskData.tasks.filter(el => el.complete === "false")
+
     let activeTasksRender = activeTasks.map(el => {
         return (
-            <li key={el.id}><label>
-                <input type="checkbox"/> {el.text}
+            <li key={el.id} onDoubleClick={editTextOfTask} className={style.taskLi}><label>
+                <input type="checkbox" onChange={toggleCheckboxTasks} /> {el.text}
             </label></li>
         )
     })
@@ -19,8 +46,8 @@ const TaskPage = (props) => {
     let doneTasks = props.taskData.tasks.filter(el => el.complete === "true")
     let doneTasksRender = doneTasks.map(el => {
         return (
-            <li key={el.id}><label>
-                <input type="checkbox" defaultChecked /> {el.text}
+            <li key={el.id} onDoubleClick={editTextOfTask} className={style.taskLi}><label>
+                <input type="checkbox" defaultChecked onChange={toggleCheckboxTasks} /> {el.text}
             </label></li>
         )
     })
@@ -29,10 +56,10 @@ const TaskPage = (props) => {
         <div>
 
             <Sidebar data={props.data.filter(el => !el.complete)} />
-            
+
             <div>
-                <h2>{props.taskData.title}</h2>
-                <p>Описание: {props.taskData.descr}</p>
+                <h2 onDoubleClick={editTitle} onBlur={editTitleOnBlur}>{props.taskData.title}</h2>
+                <p>Описание: <span onDoubleClick={editDescription}>{props.taskData.descr}</span> </p>
                 <ul>
                     {activeTasksRender}
                     {doneTasksRender}
--- src/redux/dispatches/D_DELETE_PROJECT.js
+++ src/redux/dispatches/D_DELETE_PROJECT.js
@@ -1,13 +1,17 @@
 function D_DELETE_PROJECT(action, context) {
-    let cardId = Number(action.cardId)
-    for (let i = 0; i < context._state.dataProject.length; i++) {
-        if (context._state.dataProject[i].id === cardId) {
-            let index = context._state.dataProject.indexOf(context._state.dataProject[i])
-            context._state.dataProject.splice(index, 1);
-        }
+    let isConfirmDelite = window.confirm('Вы уверены, что хотите удалить этот проект?')
+    if (isConfirmDelite) {
+        let cardId = Number(action.cardId)
+        for (let i = 0; i < context._state.dataProject.length; i++) {
+            if (context._state.dataProject[i].id === cardId) {
+                let index = context._state.dataProject.indexOf(context._state.dataProject[i])
+                context._state.dataProject.splice(index, 1);
+            }
 
+        }
+        context._callSubscriber(context._state);
     }
-    context._callSubscriber(context._state);
+
 }
 
 export default D_DELETE_PROJECT;
